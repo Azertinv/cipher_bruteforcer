86d95b07144c231da212fe72404cd2d468f99440
Azertinv <azertinv@protonmail.com>
2022-11-18 11:04:19 +0100

    started fuzzer

---
+++ fuzzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from standard_deviations import get_means_and_stdevs
+from random_cipher import random_cipher_stack, encrypt_with_cipher_stack
+
+from messages import messages
+
+def get_goal():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
